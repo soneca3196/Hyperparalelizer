9a9e5342879a52ac32e3a2eb4701fd46f7dd46b3
Isa <isa.cmorija@gmail.com>
2026-06-18 16:01:32 -0300

    pydantic

--- hyperparalelizer/server-peer_protocol.py
+++ hyperparalelizer/server-peer_protocol.py
@@ -1,1 +1,37 @@
 # mensagens externas entre redes peer e servidor (aquelas definidas no docs)
+
+# EXEMPLO DE USO: 
+# resultado = TaskResult(id_node=1, accuracy=0.98, precision=0.97, recall=0.96, f1_score=0.96, roc_auc=0.99)
+# fila_local.put(resultado)
+
+# PARA TRANSFORMAR EM JSON: model_dump_json()
+# PARA RETRANSFORMAR EM OBJETO: model_validate_json()
+
+from pydantic import BaseModel
+
+# ===== PEER -> SERVER ===== 
+
+class TaskResult(BaseModel):
+    id_node: int
+    accuracy: float
+    precision: float
+    recall: float
+    f1_score: float
+    roc_auc: float
+
+class JoinNetwork(BaseModel):
+    id_node: int
+    ip: str
+    porta: int
+
+
+# ===== SERVER -> PEER ===== 
+
+class DataLocation(BaseModel):
+    # TO DO
+    # como vamos orientar o peer para montar o dataset???
+    pass
+
+class TrainingTask(BaseModel):
+    # TO DO
+    pass
